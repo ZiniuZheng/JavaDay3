2ce955288b91a104a37e4e4316cec7b25ff6a230
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-06-12 17:49:08 +0800

    Create ArrayDemo2.java

---
+++ ArrayDemo2.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3};
+		for(int i=0;i<3;i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
