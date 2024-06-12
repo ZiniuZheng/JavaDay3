f0da55621e3abe4654ff7bf25ce05ad92c2c16be
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-06-12 17:48:39 +0800

    Create ArrayDemo.java

---
+++ ArrayDemo.java
@@ -0,0 +1,15 @@
+package Array;
+
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		for(int i=0;i<5;i++) {
+			arr[i] = i+100;
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
